4609bb9d6c4acde0b6525bedcc41361d1b6302d8
j. jillian <judith.mediosdigitales@gmail.com>
2022-12-18 23:47:09 -0600

    add controller

--- Models/Character.cs
+++ Models/Character.cs
@@ -7,11 +7,11 @@ namespace dotnet_rpg.Models
 {
     public class Character
     {
-public int Id {get; set;} = "Frodo";
-public string Name {get; set;} = "Frodo";
-public int HiPoints {get; set;} = 100;
-public int Defence {get; set;} = "Frodo"
-public int Intelligence {get; set;} = "Frodo"
-public int Id {get; set;} = "Frodo"
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HiPoints { get; set; } = 100;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
     }
 }
