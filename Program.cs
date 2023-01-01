98e98bb983877fb82ef8a3ea9396f89b96ca04c1
j. jillian <judith.mediosdigitales@gmail.com>
2023-01-02 00:30:16 +0100

    Data Context Class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ global using dotnet_rpg.Models;
 global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
